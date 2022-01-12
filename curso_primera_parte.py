f554c705c55794632245ba862d604dbe2ba2c30a
Jhamilchar <jhamil.arroyo.a@gmail.com>
2022-01-12 01:30:35 -0500

    Primer commit de tres

---
+++ curso_primera_parte.py
@@ -0,0 +1,64 @@
+# Operadores matematicos
+
+# Suma en python 
+x = 10 + 11
+
+y = 20 + 11
+
+# Resta en python
+x = 100 - 70
+
+y = 250 - 100
+
+# Multiplicacion
+x = 10 * 5
+
+y = 40 * 3
+
+# Division
+x = 15 / 5
+
+y = 100 / 20
+
+# Division Entera (Divide hasta su maximo valor y solo queda el resto)
+x = 19 // 3
+
+y = 89 // 10
+
+# Modulo 0 Resto de la division
+x = 19 % 3
+
+y = 89 % 10
+
+# Exponenciacion
+x = 2 ** 3
+
+y = 3 ** 7
+
+# Orden de los operadores
+# El resultado no es 1
+x = 2 + 4 * 2 - 9 / 3 
+
+# El resultado no es 7
+x = (2 + 4) * (2 - 9) / 3 
+
+# En la exponienciacion siempre sera de izquierda a derecha
+
+x = 2 ** 2 ** 1
+
+# Operadores booleanos
+
+# Logica Booleana
+
+# El algebra booleana fue desarrollada por George Boole, que es la base para la logica detras de las computadores y los lenguajes de programación.
+
+# Esta construida alrededor de los valores de verdad de las expresiones u objetos (Estos pueden ser True o False) y usa como base los operadores lógicos AND, OR y NOT.
+
+# Expresion booleana: Tiene un valor de verdad, de verdadero o falso.(TRUE OR FALSE)
+5 < 6 
+
+3 == 4
+
+# Los operadores sirven para trabajar con dos o mas expresiones boleanas.
+x = (5 < 6) or (3 == 4)
+print(x)
